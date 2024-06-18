a5cf36a4d0462403639421b3a091a9c142a86e0f
MnrMine <mnr_mine_agri@hotmail.com>
2024-06-18 06:33:31 +0300

    Add project files.

---
+++ Rent_A_Car/Controllers/DefaultController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Rent_A_Car.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult SearchCar()
+        {
+            return View();
+        }
+        public IActionResult CarList()
+        {
+            return View();
+        }
+    }
+}
